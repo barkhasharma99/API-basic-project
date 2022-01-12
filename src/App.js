f482ba2139dba16477340161c65e4422a74f1c51
barkha sharma <barkha.sharma55722@gmail.com>
2022-01-12 16:52:24 +0530

    basic API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      DataisLoaded: false
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch("https://api.github.com/users") // fetch the API from this link
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true
+        });
+      })
+  }
+  
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded) return <div>
+       </div>;
+
+    return (
+      <div className="App">
+        <h1> Fetch data from an api in react </h1> {
+          items.map((item) => (
+            <table>
+              <ul key={item.id} style={{ border: '2px solid green' }}>
+                User_login: {item.login},
+                User_id: {item.id},
+                User_node_id: {item.node_id}
+                User_avatar_url: {item.avatar_url}
+                User_gravatar_id: {item.gravatar_id}
+                User_url: {item.url}
+              </ul>
+            </table>
+          ))
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import About from './Components/About';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Navbar from './Components/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar/>
     <App />
+    <Home/>
+    <Contact/>
+    <About/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +23,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+export default App;
